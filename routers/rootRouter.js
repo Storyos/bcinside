0f71691deb6fb1ba49474ae11aada61aead3ae4c
Jistan7011 <qus7011@gmail.com>
2024-03-14 14:14:39 +0900

    for pull

--- routers/rootRouter.js
+++ routers/rootRouter.js
@@ -5,6 +5,7 @@ const {} = require("../controllers/postController");
 const rootRouter = express.Router();
 
 rootRouter.route("/").get((req, res) => {
+  console.log(req.cookies)
   res.status(400).render("index");
 });
 
